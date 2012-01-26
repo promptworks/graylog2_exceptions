3b4eca61472d7e5f0f964c988fef47dffc3950e0
Matt Conway <wr0ngway@yahoo.com>
2012-01-26 10:21:53 -0500

    send rack environment as custom attributes, some bugfixes

--- lib/graylog2_exceptions.rb
+++ lib/graylog2_exceptions.rb
@@ -12,10 +12,16 @@ class Graylog2Exceptions
       :local_app_name => Socket::gethostname,
       :facility => 'graylog2_exceptions',
       :max_chunk_size => 'LAN',
-      :level => 3
+      :level => 3,
+      :host => nil,
+      :short_message => nil,
+      :full_message => nil,
+      :file => nil,
+      :line => nil
     }
 
-    @args = standard_args.merge(args)
+    @args = standard_args.merge(args).reject {|k, v| v.nil? }
+    @extra_args = @args.reject {|k, v| standard_args.has_key?(k) }
     @app = app
   end
 
@@ -27,30 +33,53 @@ class Graylog2Exceptions
   def _call(env)
     begin
       # Call the app we are monitoring
-      @app.call(env)
-    rescue Exception => err
+      response = @app.call(env)
+    rescue => err
       # An exception has been raised. Send to Graylog2!
-      send_to_graylog2(err)
+      send_to_graylog2(err, env)
 
       # Raise the exception again to pass back to app.
       raise
     end
+
+    if env['rack.exception']
+      send_to_graylog2(env['rack.exception'], env)
+    end
+
+    response
   end
 
-  def send_to_graylog2 err
+  def send_to_graylog2(err, env=nil)
     begin
-      notifier = GELF::Notifier.new(@args.delete(:hostname), @args.delete(:port), @args.delete(:max_chunk_size))
-      notifier.notify!({
-				:short_message => err.message,
-        :full_message => err.backtrace.join("\n"),
-        :facility => @args.delete(:facility),
-        :level => @args.delete(:level),
-        :host => @args.delete(:local_app_name),
-        :file => err.backtrace[0].split(":")[0],
-        :line => err.backtrace[0].split(":")[1],
-			}.merge(@args))
-    rescue => i_err
+      notifier = GELF::Notifier.new(@args[:hostname], @args[:port], @args[:max_chunk_size])
+      notifier.collect_file_and_line = false
+      
+      opts = {
+          :short_message => err.message,
+          :facility => @args[:facility],
+          :level => @args[:level],
+          :host => @args[:local_app_name]
+      }
+
+      if err.backtrace && err.backtrace.size > 0
+        opts[:full_message] = err.backtrace.join("\n")
+        opts[:file] = err.backtrace[0].split(":")[0]
+        opts[:line] = err.backtrace[0].split(":")[1]
+      end
+
+      if env and env.size > 0
+        env.each do |k, v|
+          begin
+            opts["_env_#{k}"] = v.inspect
+          rescue
+          end
+        end
+      end
+      
+      notifier.notify!(opts.merge(@extra_args))
+    rescue Exception => i_err
       puts "Graylog2 Exception logger. Could not send message: " + i_err.message
     end
   end
+
 end
